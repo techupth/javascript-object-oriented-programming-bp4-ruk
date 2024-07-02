c38049161e0600c381200fe95f78e50813ee7e69
Anuruk <anuruk.chalerm@gmail.com>
2024-07-03 02:39:59 +0700

    send answer

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,40 @@
-class EmailNotification { }
+class EmailNotification { 
+    constructor(notificationId, createdTime, content, receiver) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`);
+    }
+}
 
-class SMSNotification { }
+class SMSNotification { 
+    constructor(notificationId, createdTime, content, phoneNumber) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.console = content;
+        this.phoneNumber = phoneNumber;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+}
+
+const email = new EmailNotification(
+    "email001",
+    "01/01/2024 12:00:00",
+    "This is your email.",
+    "test01@testmail.com"
+);
+
+const sms = new SMSNotification(
+    "sms001",
+    "02/01/2024 13:00:00",
+    "This is your SMS.",
+    "098-765-4321"
+);
+
+email.send();
+sms.send();
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,47 @@
-class Notification { }
+class Notification { 
+    constructor(notificationId, createdTime, content, receiver) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`);
+    }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification { 
+    constructor(notificationId, createdTime, content, receiver) {
+        super(notificationId, createdTime, content, receiver);
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`);
+    }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification { 
+    constructor(notificationId, createdTime, content, phoneNumber) {
+        super(notificationId, createdTime, content);
+        this.phoneNumber = phoneNumber;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+}
+
+const email = new EmailNotification(
+    "email001",
+    "01/01/2024 12:00:00",
+    "This is your email.",
+    "test01@testmail.com"
+);
+
+const sms = new SMSNotification(
+    "sms001",
+    "02/01/2024 13:00:00",
+    "This is your SMS.",
+    "098-765-4321"
+);
+
+email.send();
+sms.send();
